bcd0199eed6273c3d0488ceaecd503ad53d38e3d
Parth Bhattad <bhattadparth20@gmail.com>
2023-07-14 18:52:56 +0530

    deploy

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -12,7 +12,7 @@ export default function ContactUs(){
                             <div className="bg-[#0b0c10] text-[#66fcf1] text-5xl w-16 rounded-full p-2">
                             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.832 2.688A4.056 4.056 0 0 0 8.02 1.5h-.04a4.056 4.056 0 0 0-4 4c-.013.75.198 1.487.606 2.117L7.734 14h.533l3.147-6.383c.409-.63.62-1.367.606-2.117a4.056 4.056 0 0 0-1.188-2.812zM7.925 2.5l.082.01.074-.01a3.075 3.075 0 0 1 2.941 3.037 2.74 2.74 0 0 1-.467 1.568l-.02.034-.017.035L8 12.279l-2.517-5.1-.017-.039-.02-.034a2.74 2.74 0 0 1-.467-1.568A3.074 3.074 0 0 1 7.924 2.5zm.612 2.169a1 1 0 1 0-1.112 1.663 1 1 0 0 0 1.112-1.663zM6.87 3.837a2 2 0 1 1 2.22 3.326 2 2 0 0 1-2.22-3.326z"></path></svg>
                             </div>
-                            <div className="mt-4 font-thin text-[#66fcf1] text-3xl mb-4 tracking-wide uppercase"><a href="https://goo.gl/maps/vZ1DBy7iJZjybDnT7" target="_blank">Address</a></div>
+                            <div className="mt-4 font-thin text-[#66fcf1] text-3xl mb-4 tracking-wide uppercase"><a href="https://goo.gl/maps/vZ1DBy7iJZjybDnT7" target="_blank" rel="noreferrer">Address</a></div>
                             <div className="text-center mt-2 text-[#c6c5c7] text-sm lg:px-8">44, Shiv Darshan Rd, Parvati, विद्या नगरी, Vidya Nagari, Parvati Paytha, Pune, Maharashtra 411009</div>
                         </div>
                     </div>
