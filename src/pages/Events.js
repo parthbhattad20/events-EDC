69efc1254b91e00da14807429498c7740b02d8af
Parth Bhattad <bhattadparth20@gmail.com>
2023-09-07 12:18:01 +0530

    done

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -7,7 +7,7 @@ import Footer from "../components/Footer"
 // import Data from "../components/CardData"
 import EventCard from "../components/EventCard"
 import TEventCard from "../components/TEventCard"
-import image from "../assets/bg-image.jpg"
+import video from "../assets/SV.mp4"
 
 export default function Events(){
 
@@ -56,31 +56,36 @@ export default function Events(){
     // }
     return(
         <div>
-            <Navbar />
-            <div className="">
-                <div className="relative bg-[#0b0c10]">
-                    <img src={image} alt="events" className="w-full h-screen  bg-no-repeat bg-cover object-cover blur"/>
-                    <div className="absolute top-[70%] left-1 md:top-17 md:left-17 w-42 md:w-auto">
-                        <h1 className="text-center text-6xl lg:text-7xl xl:text-8xl pb-2 md:pb-4 text-[#66fcf1] font-bold uppercase tracking-wider">Events</h1>
-                        <p className="text-normal text-center border-t border-[#66fcf1] p-0 pt-2 md:pt-4 tracking-wide"></p>
-                    </div>
-                </div>
-                <div className="p-12 pt-2">
-                    <div>
-                        <h2 className="text-3xl max-w-[1250px] pl-7 mx-auto md:text-4xl xl:text-5xl py-8 text-[#66fcf1] font-thin uppercase tracking-wider">	</h2>
-                        <div className="max-w-[1250px] mx-auto px-8 py-4">
-                          <TEventCard/>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 className="text-3xl max-w-[1250px] pl-7 mx-auto md:text-4xl xl:text-5xl py-8 text-[#66fcf1] font-thin uppercase tracking-wider">	</h2>
-                        <div className="max-w-[1250px] mx-auto px-8 py-4">
-                          <EventCard/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
+      <Navbar />
+      <div className="">
+        <div className="relative bg-[#0b0c10]">
+          <div className="video-container h-[650px]">
+            <video autoPlay loop muted className="w-full h-full  object-fit">
+              <source src={video} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="absolute top-[85%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-42 md:w-auto">
+            <h1 className="text-center text-6xl lg:text-7xl xl:text-8xl pb-2 md:pb-4 text-[#66fcf1] font-bold uppercase tracking-wider">Events</h1>
+            <p className="text-normal text-center border-t border-[#66fcf1] p-0 pt-2 md:pt-4 tracking-wide"></p>
+          </div>
         </div>
+        <div className="p-12 pt-2">
+          <div>
+            <h2 className="text-3xl max-w-[1250px] pl-7 mx-auto md:text-4xl xl:text-5xl py-8 text-[#66fcf1] font-thin uppercase tracking-wider">	</h2>
+            <div className="max-w-[1250px] mx-auto px-8 py-4">
+              <TEventCard />
+            </div>
+          </div>
+          <div>
+            <h2 className="text-3xl max-w-[1250px] pl-7 mx-auto md:text-4xl xl:text-5xl py-8 text-[#66fcf1] font-thin uppercase tracking-wider">	</h2>
+            <div className="max-w-[1250px] mx-auto px-8 py-4">
+              <EventCard />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
     )
 }
